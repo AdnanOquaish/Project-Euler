0a7d21d8857d175d91af4c512c42f955feb0d6f9
CyberKnight <adnoquaish@gmail.com>
2015-07-06 13:50:31 +0530

    Create P16.cpp

---
+++ P16.cpp
@@ -0,0 +1,28 @@
+package problems;
+
+import java.math.BigInteger;
+//import java.util.Scanner;
+import java.io.*;
+
+public class P16 {
+
+	public static void main(String args[]) throws IOException
+	{
+		BigInteger b = new BigInteger("1");
+		BigInteger a =  new BigInteger("2");
+		int i;
+		for(i=0;i<1000;i++)
+		{
+			b=b.multiply(a);			
+		}
+		String str = b.toString();
+		System.out.println(str);
+		int n = str.length();
+		int sum = 0;
+		for(i=0;i<n;i++)
+		{
+			sum = sum + (str.charAt(i)-'0');
+		}
+		System.out.println(sum);
+	}
+}
